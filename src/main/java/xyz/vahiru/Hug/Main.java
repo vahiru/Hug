8274a82991889cdbe2f46b554f54bab2fb7e7b97
Mashirl <i@mashirl.com>
2025-01-17 00:08:28 +0800

    chore: refine file structure & configure maven

---
+++ src/main/java/xyz/vahiru/Hug/Main.java
@@ -0,0 +1,38 @@
+package xyz.vahiru.Hug;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HugService hugService = new HugService();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("选择模式（client/server）：");
+        String mode = scanner.nextLine();
+
+        if ("client".equalsIgnoreCase(mode)) {
+            System.out.print("要抱抱谁（输入IP地址）：");
+            String ip = scanner.nextLine();
+            int port = 8520; // 可以根据需要修改端口号
+
+            try {
+                hugService.sendHugRequest(ip, port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } else if ("server".equalsIgnoreCase(mode)) {
+            int port = 8520; // 可以根据需要修改端口号
+
+            try {
+                hugService.startHugServer(port);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("无效的模式选择。");
+        }
+    }
+}
